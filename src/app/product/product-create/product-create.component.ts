791177bd46b53e987b9890a68cd4acf85e27b100
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2023-11-30 13:11:04 -0300

    ♻️ refact: add create product data communication with api

--- src/app/product/product-create/product-create.component.ts
+++ src/app/product/product-create/product-create.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { IProduct } from 'src/app/model/IProduct.model';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-product-create',
@@ -10,10 +12,31 @@ export class ProductCreateComponent {
   product: IProduct = {
     name: '',
     price: 0,
+    description: '',
+    image: '',
+    quantity: 0,
     due_at: '',
-  }
+  };
+
+  constructor(private productService: ProductsService, private router: Router) {}
 
   create() {
-    console.log('Novo produto criado', this.product)
+    if (this.validateProduct()) {
+      this.productService.createProduct(this.product).subscribe(() => {
+        this.router.navigate(['/product']);
+      });
+    } else {
+      console.error('Preencha todos os campos obrigatórios');
+    }
+  }
+
+  private validateProduct(): boolean {
+    return (
+      this.product.name.trim() !== '' &&
+      this.product.price > 0 &&
+      this.product.description.trim() !== '' &&
+      this.product.image.trim() !== '' &&
+      this.product.quantity >= 0
+    );
   }
 }
