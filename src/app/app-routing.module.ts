824715e419bcc56f8ed4d67b70b8c653390dcdc8
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2023-12-01 14:41:55 -0300

    ✨ feat: add new edit routes and adjust home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientRegisterComponent } from './client/client-register/client-register.component';
+import { ClientEditComponent } from './client/client-edit/client-edit.component';
 import { ClientListComponent } from './client/client-list/client-list.component';
 import { HomeComponent } from './home/home.component';
 import { ProductCreateComponent } from './product/product-create/product-create.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductEditComponent } from './product/product-edit/product-edit.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: 'client/register', component: ClientRegisterComponent },
   { path: 'client', component: ClientListComponent },
+  { path: 'client/update/:id', component: ClientEditComponent },
   { path: 'product', component: ProductListComponent },
-  { path: 'product/create', component: ProductCreateComponent }
+  { path: 'product/create', component: ProductCreateComponent },
+  { path: 'product/update/:id', component: ProductEditComponent },
 ];
 
 @NgModule({
