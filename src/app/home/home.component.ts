93a696705df036a991b5149cd4a853a1ff89d501
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2023-12-01 14:43:10 -0300

    ♻️ refact: add client cards to home page and create navigation buttons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IProduct as ProductCard } from '../model/IProduct.model';
+import { IClient as ClientCard } from '../model/IClient.model';
 import { ProductsService } from '../services/products.service';
+import { ClientsService } from '../services/clients.service';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +11,9 @@ import { ProductsService } from '../services/products.service';
 })
 export class HomeComponent {
   cardItems: ProductCard[] = []
+  clientCardItems: ClientCard[] = []
 
-  constructor(private productService: ProductsService) {
-
-  }
+  constructor(private productService: ProductsService, private clientService: ClientsService) {}
 
   ngOnInit(): void {
     this.getData();
@@ -21,6 +22,9 @@ export class HomeComponent {
   getData(): void {
     this.productService.getProducts().subscribe(products => {
       this.cardItems = products;
+    })
+    this.clientService.getClients().subscribe(clients => {
+      this.clientCardItems = clients;
     })
   }
 }
