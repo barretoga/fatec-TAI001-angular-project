445a9e13b9aa26e835470121361bdee5ed811399
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2023-12-01 14:44:52 -0300

    🐛 fix: adjusted card visualization

--- src/app/template/card/card.component.ts
+++ src/app/template/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { ProductsService } from 'src/app/services/products.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -6,11 +8,20 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-  @Input() id?: string
+  @Input() id?: number
   @Input() name?: string
   @Input() image?: string
   @Input() quantity?: number
   @Input() price?: number
   @Input() description?: string
   @Input() due_at?: string | Date
+
+  constructor(private productService: ProductsService, private router: Router) {}
+
+  deleteProduct(id: number | undefined): void {
+    if (id) {
+      this.productService.deleteProduct(id).subscribe(() => {
+        this.router.navigate(['/product']);})
+    }
+  }
 }
