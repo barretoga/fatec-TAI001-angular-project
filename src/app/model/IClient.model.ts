f0b6e871faa7d7d87fbde242d1793211c1836b8d
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2023-12-01 14:43:31 -0300

    ✏️ fix: improve interface construction creating IClientAddress

--- src/app/model/IClient.model.ts
+++ src/app/model/IClient.model.ts
@@ -1,15 +1,17 @@
+export interface IClientAddress {
+  street: string,
+  number: string,
+  complement: string,
+  neighborhood: string,
+  city: string,
+  state: string,
+  zip_code: string
+}
+
 export interface IClient {
   id?: number,
   name: string,
   email: string,
   phone: string,
-  address: {
-    street: string,
-    number: string,
-    complement: string,
-    neighborhood: string,
-    city: string,
-    state: string,
-    zip_code: string
-  },
+  address: IClientAddress
 }
